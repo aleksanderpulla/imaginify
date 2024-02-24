da580bac60add5faf93b29895ad0c44db75b9925
Aleksandër Pulla <aleksanderpulla@gmail.com>
2024-02-24 16:50:08 +0100

    Image Details, Update, Delete functionalities

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -172,9 +172,9 @@ const TransformationForm = ({
           [fieldName === "prompt" ? "prompt" : "to"]: value,
         },
       }));
+    }, 1000)();
 
-      return onChangeField(value);
-    }, 1000);
+    return onChangeField(value);
   };
 
   const onTransformHandler = async () => {
@@ -216,6 +216,7 @@ const TransformationForm = ({
                 onValueChange={(value: any) =>
                   onSelectFieldHandler(value, field.onChange)
                 }
+                value={field.value}
               >
                 <SelectTrigger className="select-field">
                   <SelectValue placeholder="Select size" />
--- components/shared/TransformedImage.tsx
+++ components/shared/TransformedImage.tsx
@@ -1,19 +1,32 @@
 "use client";
-import { dataUrl, debounce, getImageSize } from "@/lib/utils";
-import { CldImage } from "next-cloudinary";
+import { dataUrl, debounce, download, getImageSize } from "@/lib/utils";
+import { CldImage, getCldImageUrl } from "next-cloudinary";
 import { PlaceholderValue } from "next/dist/shared/lib/get-img-props";
 import Image from "next/image";
+import { title } from "process";
 
 const TransformedImage = ({
   image,
   type,
-  title,
   transformationConfig,
   isTransforming,
   setIsTransforming,
   hasDownload = false,
 }: TransformedImageProps) => {
-  const downloadHandler = () => {};
+  const downloadHandler = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    download(
+      getCldImageUrl({
+        width: image?.width,
+        height: image?.height,
+        src: image?.publicId,
+        ...transformationConfig,
+      }),
+      title
+    );
+  };
 
   return (
     <div className="flex flex-col gap-4">
@@ -49,7 +62,7 @@ const TransformedImage = ({
             onError={() => {
               debounce(() => {
                 setIsTransforming && setIsTransforming(false);
-              }, 8000);
+              }, 8000)();
             }}
             {...transformationConfig}
           />
@@ -58,10 +71,11 @@ const TransformedImage = ({
             <div className="transforming-loader">
               <Image
                 src="/assets/icons/spinner.svg"
-                alt="Transforming"
+                alt="spinner"
                 width={50}
                 height={50}
               />
+              <p className="text-white/80">Please wait...</p>
             </div>
           )}
         </div>
